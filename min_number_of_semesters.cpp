b6789d5fbb906527a8b7b8a0187bd62b9001a7ed
Sunanda Roy <39713435+sunandaroy0516@users.noreply.github.com>
2018-11-25 16:47:24 +0530

    Initial version
    Exhaustive implementation of binary search tree.
    Find minimum no. of semesters required to finish all courses with prerequisites.

---
+++ min_number_of_semesters.cpp
@@ -0,0 +1,11 @@
+/* Given a map containing courses and the list of prerequisites for that course
+in no particular order, determine the least number of semesters to finish all
+the courses. You have to take all prerequisites before you take a given
+course.
+
+Eg.
+Calculus : English, Math2
+Math2 : Math 1, Arabic, English
+Math1 : English
+English: <>
+Arabic: <> */
