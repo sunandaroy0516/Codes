3bdb2c04acd59c60b61a85414b41835a0555190c
Sunanda Roy <39713435+sunandaroy0516@users.noreply.github.com>
2018-12-20 21:35:48 +0530

    Updated timer_callback

--- timer_callback.c
+++ timer_callback.c
@@ -8,3 +8,72 @@ I could say, Callback-function_1 should be called after 20 secs and callback-fun
 we can assume, from the start time, after 20 secs the first callback-function would be called... 
 
 How all these callback functions are put into a queue (the one which is going to be called soon has to be first...) and how they have been called ? How the timer invokes those callback-functions ? How do we pass an arguments to the callback-functions in this case...? */
+
+#include "timer_callback.h"
+
+void event_handler(unsigned int latency, unsigned int eventID)
+{
+	time_t start_time = time();
+	size_t timer_cnt;
+	
+	while (time() - start_time < latency)
+	{
+		time_t diff = latency - (time() - start_time);
+		printf("Time remaining: %u secs\n", diff);
+	}
+	
+	timer_cnt = sizeof(timer_callbacks)/sizeof(void *);
+	
+	if(eventID < timer_cnt)
+	{
+		timer_callbacks[eventID];
+	}
+}
+
+void timer_10_secs(unsigned int duration)
+{
+	printf("Ran first timer for %u seconds.\n", duration);
+}
+
+void timer_20_secs(unsigned int duration)
+{
+	printf("Ran second timer for %u seconds.\n", duration);
+}
+
+void timer_30_secs(unsigned int duration)
+{
+	printf("Ran third timer for %u seconds.\n", duration);
+}
+
+void (*timer_callbacks[3])() = {timer_10_secs, timer_20_secs, timer_30_secs};
+
+void register_callbacks_to_eventID(unsigned int latency)
+{
+	switch(latency)
+	{
+		case 10:
+			event_handler(10, 0);
+			break;
+			
+		case 20:
+			event_handler(20, 1);
+			break;
+			
+		case 30:
+			event_handler(30, 2);
+			break;
+			
+		default:
+			break;
+	}
+}
+
+int main()
+{
+	register_callbacks_to_eventID(10);
+	register_callbacks_to_eventID(20);
+	register_callbacks_to_eventID(30);
+	
+	return SUCCESS;
+}
+
--- timer_callback.h
+++ timer_callback.h
@@ -8,3 +8,20 @@ I could say, Callback-function_1 should be called after 20 secs and callback-fun
 we can assume, from the start time, after 20 secs the first callback-function would be called... 
 
 How all these callback functions are put into a queue (the one which is going to be called soon has to be first...) and how they have been called ? How the timer invokes those callback-functions ? How do we pass an arguments to the callback-functions in this case...? */
+
+#ifndef __TIMER_CALLBACK__
+#define __TIMER_CALLBACK__
+
+#include <stdio.h>
+#include <cstdlib>
+#include <time.h>
+
+#include "error_codes.h"
+
+void event_handler(unsigned int);
+void timer_10_secs();
+void timer_20_secs();
+void timer_30_secs();
+void register_callbacks(unsigned int, void (**)());
+
+#endif
