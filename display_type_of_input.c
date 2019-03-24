4ac02f31c322c43ca97e470c597b519e78ccaee5
Sunanda Roy <39713435+sunandaroy0516@users.noreply.github.com>
2019-03-24 12:02:58 +0530

    Update display_type_of_input.c

--- display_type_of_input.c
+++ display_type_of_input.c
@@ -13,24 +13,6 @@ hello123	This is something else.
 
 int main()
 {
-    printf("Hello, World!\n");
-    int ctr = 0;
-    char a[100];
-    scanf("%s", a);
-
-    int b = atoi(a);
-    float c = atof(a);
-    
-    for(int i = 0; a[i] != '\0'; i++){
-        if(!(a[i] >= 'a' && a[i] <= 'z' || a[i] >= 'A' && a[i] <= 'Z'))
-        {
-            ctr++;#include <stdio.h>
-#include <limits.h>
-#include <stdlib.h>
-
-int main()
-{
-    printf("Hello, World!\n");
     int ctr = 0;
     char a[100];
     scanf("%s", a);
@@ -47,27 +29,6 @@ int main()
     }
     
 	if(b != 0){
-		if(b == c){
-			printf("This is an integer.\n");
-		}
-		else{
-			printf("float");
-		}
-	}
-	else if(ctr){
-        printf("This is something else.\n");
-    }
-    else{
-		printf("This is a string.\n");
-	}
-
-    return 0;
-}
-            break;
-        }
-    }
-    
-	if(!b){
 		if(b == c){
 			printf("This is an integer.\n");
 		}
